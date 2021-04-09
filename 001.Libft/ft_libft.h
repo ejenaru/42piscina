65b1b1653690a5f0bc99dd8acd034056b74452d4
Irene Diaz fossaty <idiaz-fo@c1r12s2.42madrid.com>
2021-04-09 17:49:04 +0200

    source folder

--- 001.Libft/ft_libft.h
+++ 001.Libft/ft_libft.h
@@ -6,7 +6,7 @@
 /*   By: idiaz-fo <idiaz-fo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 12:06:07 by idiaz-fo          #+#    #+#             */
-/*   Updated: 2021/04/07 14:08:45 by idiaz-fo         ###   ########.fr       */
+/*   Updated: 2021/04/09 17:26:52 by idiaz-fo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 # include <stdlib.h>
 # include <string.h>
 
+#include <stdio.h>
+
 int		ft_atoi(char *str);
+size_t strlen(const char *s);
 #endif
--- 001.Libft/src/ft_strlen.c
+++ 001.Libft/src/ft_strlen.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: idiaz-fo <idiaz-fo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/07 12:03:47 by idiaz-fo          #+#    #+#             */
-/*   Updated: 2021/04/09 14:21:45 by idiaz-fo         ###   ########.fr       */
+/*   Created: 2021/04/09 17:24:09 by idiaz-fo          #+#    #+#             */
+/*   Updated: 2021/04/09 17:36:39 by idiaz-fo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_libft.h"
+#include <stdlib.h>
 
-int	main(void)
+size_t	ft_strlen(const char*s)
 {
-	int i;
-	int mine;
-  char buffer[256];
-  printf ("Enter a number: ");
-  fgets (buffer, 256, stdin);
-  i = atoi (buffer);
-  mine = ft_atoi (buffer);
-  printf ("Original  %d. Mine  %d.\n",i,mine);
-	
-	return (0);
+	return(0);
 }
