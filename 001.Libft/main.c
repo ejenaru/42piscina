bfa8090d204c7e683765cfdf72c89c2cbddbb277
Irene Diaz fossaty <idiaz-fo@c1r12s2.42madrid.com>
2021-04-09 17:53:51 +0200

    conflicts

--- 001.Libft/main.c
+++ 001.Libft/main.c
@@ -6,11 +6,7 @@
 /*   By: idiaz-fo <idiaz-fo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 12:03:47 by idiaz-fo          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/04/09 14:21:45 by idiaz-fo         ###   ########.fr       */
-=======
-/*   Updated: 2021/04/07 23:16:06 by idiaz-fo         ###   ########.fr       */
->>>>>>> c1359b706f78ca700b1401c0327993511eb328a0
+/*   Updated: 2021/04/09 17:53:02 by idiaz-fo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +14,6 @@
 
 int	main(void)
 {
-<<<<<<< HEAD
 	int i;
 	int mine;
   char buffer[256];
@@ -27,9 +22,5 @@ int	main(void)
   i = atoi (buffer);
   mine = ft_atoi (buffer);
   printf ("Original  %d. Mine  %d.\n",i,mine);
-=======
-	printf("Hello world!\n");
->>>>>>> c1359b706f78ca700b1401c0327993511eb328a0
-	
 	return (0);
 }
